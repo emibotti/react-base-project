987c195ae1838afbe78ce6cb1b54b6139b5dfab8
Emiliano Botti <emiliano@xmartlabs.com>
2019-08-31 19:03:58 -0300

    Added Semantic UI and Webpack loaders for that.

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,12 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react'
 import ReactDOM from 'react-dom'
+import 'semantic-ui-css/semantic.min.css'
+import { Header } from 'semantic-ui-react'
 
 class App extends React.Component {
   render() {
-    return <h1>Hello, world!</h1>
+    return <Header as="h1">Added Semantic UI!</Header>
   }
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,26 @@ module.exports = {
           loader: 'babel-loader',
         },
       },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+      {
+        test: /(\.css$)/,
+        loaders: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000',
+      },
+      {
+        test: /\.jpe?g$|\.gif$|\.ico$|\.png$|\.svg$/,
+        use: 'file-loader?name=[name].[ext]?[hash]',
+      },
     ],
   },
   plugins: [

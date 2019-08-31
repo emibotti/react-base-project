ea890d63957bbec1034d8952f3ebbee566983ae5
Emiliano Botti <emiliano@xmartlabs.com>
2019-08-31 18:08:26 -0300

    Webpack working

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,52 +2,27 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
-    node: true
+    node: true,
   },
-  extends: ["airbnb"],
+  extends: ['airbnb'],
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 2018,
-    sourceType: "module"
+    sourceType: 'module',
   },
-  plugins: ["react"],
+  plugins: ['react'],
+  settings: {
+    'import/resolver': {
+      'babel-module': {},
+    },
+  },
   rules: {
-    align: false,
-    "array-type": false,
-    "arrow-parens": [true, "ban-single-arg-parens"],
-    "import-name": false,
-    "interface-name": false,
-    "max-classes-per-file": false,
-    "max-line-length": [
-      true,
-      {
-        limit: 120,
-        ignorePattern: "^import "
-      }
-    ],
-    "no-console": false,
-    "no-namespace": false,
-    "no-unused-variable": true,
-    "no-var-requires": false,
-    "object-shorthand-properties-first": false,
-    semicolon: false,
-    "space-before-function-paren": false,
-    "strict-boolean-expressions": false,
-    "ter-arrow-parens": false,
-    "ter-indent": false,
-    "trailing-comma": [true, { multiline: { typeLiterals: "ignore" } }],
-    "variable-name": [
-      true,
-      "ban-keywords",
-      "check-format",
-      "allow-leading-underscore",
-      "allow-pascal-case"
-    ]
-  }
-};
+    semi: 0,
+  },
+}
---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/prefer-stateless-function */
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends React.Component {
+  render() {
+    return <h1>Hello, world!</h1>
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('mount'))

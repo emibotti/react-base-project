950a1b8d9bdaa2924ba438a15d44adce41aa861c
Emiliano Botti <emiliano@xmartlabs.com>
2019-08-31 20:16:28 -0300

    Added Semantic UI LESS

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,12 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react'
 import ReactDOM from 'react-dom'
-import 'semantic-ui-css/semantic.min.css'
+import 'semantic-ui-less/semantic.less'
 import { Header } from 'semantic-ui-react'
 
 class App extends React.Component {
   render() {
-    return <Header as="h1">Added Semantic UI!</Header>
+    return <Header as="h1">Semantic UI LESS!</Header>
   }
 }
 
